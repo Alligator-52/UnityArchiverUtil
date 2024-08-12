f2373d0c285f8ea70c4e15286867b6210eccd00e
Ayush Ankit Patnaik <ayushankitpatnaik99@gmail.com>
2024-08-12 18:19:38 +0530

    added some executing code

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,49 @@
 use std::env;
 use std::fs;
 use std::io::{self,Write};
-use std::path::{Path, PathBuf};
-use std::process::Command;
-use flate2::write::GzEncoder;
-use flate2::Compression;
-use std::fs::File;
-use std::io::prelude::*;
-use zip::write::FileOptions;
+use std::path::{Path};
+// use std::process::Command;
+// use flate2::write::GzEncoder;
+// use flate2::Compression;
+// use std::fs::File;
+// use std::io::prelude::*;
+// use zip::write::FileOptions;
 
 const REQUIRED_FOLDERS: [&str; 4] = ["Assets","Packages", "ProjectSettings", "UserSettings"];
-fn main() {
-    if(is_unity_dir(Path::new(r"D:\Dx\Project\HoloLens Runtime - Testing")))
+fn main() -> io::Result<()> 
+{
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 0 
     {
-        println!("It is working");
+        println!("Empty!");
+        return Ok(());
+    }  
+
+    let path = Path::new(&args[1]);
+
+    if !path.is_dir()
+    {
+        println!("The provided path is not a direcrtory");
+        return Ok(());
+    } 
+
+    if !is_unity_project(path)
+    {
+        println!("The provided directory is not a unity project!");
+        return  Ok(());
     }
-    else{
-        println!("Not working!");
-    }
+
+    let backup_dir = path.join("Archive");
+    copy_project_files(path, &backup_dir);
+    return Ok(());
 }
 
-fn is_unity_dir(dir: &Path) -> bool
+fn is_unity_project(dir: &Path) -> bool
 {
     return REQUIRED_FOLDERS.iter().all(|&d| dir.join(d).is_dir());
+}
+
+fn copy_project_files(source: &Path, destination: &Path)
+{
+    println!("This method is working");
 }
