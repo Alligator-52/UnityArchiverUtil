1729696fccd928dec8021cf55fcc3aa9ff2e1ef4
Alligator-52 <ayushankitpatnaik99@gmail.com>
2024-08-16 08:32:16 +0530

    Added zipping with Zstd compression

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::env;
 use std::fs;
+use std::fs::File;
 use std::io::{self,Write};
 use std::path::{Path, PathBuf};
 use colored::*;
+use zip::write::FileOptions;
+use zip::CompressionMethod;
+use zip::ZipWriter;
 
+//to do, add option to delete og files
 const REQUIRED_FOLDERS: [&str; 4] = ["Assets","Packages", "ProjectSettings", "UserSettings"];
 fn main() -> io::Result<()> 
 {
@@ -42,12 +47,12 @@ fn main() -> io::Result<()>
 
     let backup_dir = path.join(name + "_Archive");
     //println!("\narchive directory name: {:?}", &backup_dir);
-    copy_project_files(path, &backup_dir);
+    let _ = copy_project_files(path, &backup_dir);
 
     let mut zip_input: String = String::new();
 
     loop
-    {
+    {   
         println!("do you want to zip the archived folder? y/n \n");
         match io::stdin().read_line(&mut zip_input)
         {
@@ -57,8 +62,8 @@ fn main() -> io::Result<()>
                 {
                     "yes"|"y" => 
                     {
-                        println!("\nThe folder will be zipped");
-                        zip_archived_folder();
+                        println!("\nZipping folder {}", backup_dir.file_name().expect("Cannot extract directory name").to_string_lossy().blue());
+                        let _ = zip_archived_folder(&backup_dir);
                         break;
                     },
                     "no"|"n" =>
@@ -132,17 +137,85 @@ fn replicate_folder_recursively(source: &Path, destination: &Path) -> io::Result
 
         if entry_path.is_dir()
         {
-            replicate_folder_recursively(&entry_path, &destination_path);
+            let _ = replicate_folder_recursively(&entry_path, &destination_path);
         }
         else
         {
-            fs::copy(&entry_path, &destination_path);
+            let _ = fs::copy(&entry_path, &destination_path);
         }
     }
     return Ok(());
 }
 
-fn zip_archived_folder() -> io::Result<()>
+fn zip_archived_folder(folder_to_zip :&PathBuf) -> io::Result<()>
 {
+    let zip_file_path = folder_to_zip.with_extension("zip");
+    let zip_file = File::create(&zip_file_path)?;
+    let mut zip = ZipWriter::new(zip_file);
+    let zip_option = FileOptions::default().compression_method(CompressionMethod::ZSTD);
+
+    for item in fs::read_dir(folder_to_zip)? 
+    {
+        let item = item?;
+        let path = item.path();
+
+        if path.is_file()
+        {
+            let file_name = match path.strip_prefix(folder_to_zip) 
+            {
+                Ok(p) => p.to_str().unwrap(),
+                Err(e) => {
+                    eprintln!("Failed to strip prefix: {}", e);
+                    return Err(io::Error::new(io::ErrorKind::Other, "Prefix stripping failed"));
+                }
+            };
+            let mut file = File::open(&path)?;
+
+            zip.start_file(file_name, zip_option)?;
+
+            io::copy(&mut file, &mut zip)?;
+        }
+        else if path.is_dir()
+        {
+            zip_directory(&path, &mut zip, &zip_option, folder_to_zip)?;
+        }
+    }
+    zip.finish()?;
+    println!
+    (
+        "{} success ziped to {}", 
+        folder_to_zip.to_string_lossy().yellow(), 
+        zip_file_path.to_string_lossy().green()
+    );
+
+    return Ok(());
+}
+
+fn zip_directory(directory: &Path, zip: &mut ZipWriter<File>, zip_option : &FileOptions, base_directory: &Path) -> io::Result<()>
+{
+    for item in fs::read_dir(directory)?
+    {
+        let item = item?;
+        let path = item.path();
+
+        if path.is_file()
+        {
+            let file_name = match path.strip_prefix(base_directory) 
+            {
+                Ok(p) => p.to_str().unwrap(),
+                Err(e) => {
+                    eprintln!("Failed to strip prefix: {}", e);
+                    return Err(io::Error::new(io::ErrorKind::Other, "Prefix stripping failed"));
+                }
+            };
+            let mut file = File::open(&path)?;
+            let _ = zip.start_file(file_name, *zip_option)?;
+            let _ = io::copy(&mut file, zip);
+        }
+        else if path.is_dir()
+        {
+            zip_directory(&path, zip, zip_option, base_directory)?;    
+        }
+    }
     return Ok(());
 }
